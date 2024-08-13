bad0192b3c538212fd50b07f9b8095ae161eb7e8
Giovanni Dunica <88715954+GiovanyUK@users.noreply.github.com>
2024-08-13 22:54:35 +0300

    Add files via upload

---
+++ AbstractClassChallenge/src/dev/lpa/ArtObject.java
@@ -0,0 +1,18 @@
+package dev.lpa;
+
+import java.sql.SQLOutput;
+
+public class ArtObject extends ProductForSale {
+
+
+    public ArtObject(String type, double price, String description) {
+        super(type, price, description);
+    }
+
+    @Override
+    public void showDetails() {
+        System.out.println("This " + type + " is a cool art ");
+        System.out.printf("The price of the piece is $%6.2f %n", price);
+        System.out.println(description);
+    }
+}
